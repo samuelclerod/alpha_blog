a08415a34c047d9a5803c89ac743ef85f937ee51
Samuel Rodrigues <samuelclerod@gmail.com>
2022-06-10 09:49:39 -0300

    test: article controllers

---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,51 @@
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @article = articles(:minitest)
+  end
+
+  test "should get articles index" do
+    get articles_url
+    assert_equal "index", @controller.action_name
+    assert_match "Articles", @response.body
+    # assert_match @article.title, @response.body
+    assert_response :success
+  end
+
+  test "should show article" do
+    get articles_url(@article)
+    assert_response :success
+  end
+
+  test "should add a new article" do
+    assert_difference('Article.count') do
+      post articles_url, params: { article: { title: "Test Title", description: "Test Text Description" } }
+    end
+    assert_redirected_to article_url(Article.last)
+  end
+
+  test "should not add a new article" do
+    assert_no_difference('Article.count') do
+      post articles_url, params: { article: { title: "", description: "" } }
+    end
+    assert_response :success
+  end
+
+  test "should delete an article" do 
+    assert_difference('Article.count', -1) do
+      delete article_url(@article)
+    end
+    assert_redirected_to articles_url
+  end
+
+  test "should update an article" do
+    patch article_url(@article), params: { article: { title: "Test Title updated", description: "Test Text updated" } }
+    assert_redirected_to article_url(@article)
+  end
+
+  test "should not update an article" do
+    patch article_url(@article), params: { article: { title: "", description: "" } }
+    assert_response :success
+  end
+  
+end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+
+  setup do
+    @article = articles(:minitest)
+  end
+
   test "should save article without erros" do
     article = Article.new(title: "Test Title", description: "Test Text Description")
     assert article.save
   end
-  
+
   test "should not save article without title and description" do
     article = Article.new
     assert_not article.save, "Saved the article without a title"
   end  
-  
+
   test "should not save article without title" do
     article = Article.new
     article.description = "some description"
@@ -29,7 +34,7 @@ class ArticleTest < ActiveSupport::TestCase
     article.title = "some title"
     assert_not article.save, "Saved the article without a description"
   end
-  
+
   test "should not save article with description title lenght less than 10" do
     article = Article.new
     article.title = "some title"
@@ -37,5 +42,43 @@ class ArticleTest < ActiveSupport::TestCase
     assert_not article.save, "Saved the article with small description"
   end
 
+  test "should edit an article without erros" do
+    @article.title = "Test Title updated"
+    @article.description = "Test Text updated"
+    assert @article.valid?
+    assert @article.save
+  end
+
+  test "should not edit an article with invalid name" do
+    @article.title = ""
+    @article.description = "Test Text updated"
+    assert_not @article.valid?
+    assert_not @article.save
+  end
+
+  test "should not edit an article with small lenght name" do
+    @article.title = "test"
+    @article.description = "Test Text updated"
+    assert_not @article.valid?
+    assert_not @article.save
+  end
+
+  test "should not edit an article with invalid description" do
+    @article.title = "Test Title updated"
+    @article.description = ""
+    assert_not @article.valid?
+    assert_not @article.save
+  end
+
+  test "should not edit an article with small leght description" do
+    @article.title = "Test Title updated"
+    @article.description = "small"
+    assert_not @article.valid?
+    assert_not @article.save
+  end
+
+  test "should retrieve 2 articles" do
+    assert_equal 2, Article.count, "Retrieved more than 2 articles"	
+  end
 
 end
